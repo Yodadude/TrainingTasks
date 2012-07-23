26c318cc284eda922059810ce9c1e8286c04ff86
John Byrne <jbyrne@inlogik.com>
2012-07-23 18:22:41 +1000

    Initial commit

--- TrainingTasks3/Menu.cs
+++ TrainingTasks3/Menu.cs
@@ -1,6 +1,49 @@
+using System;
+using System.Collections.Generic;
+
 namespace TrainingTasks3
 {
     public class Menu
     {
-    }
+
+		public MenuConfig Config(Func<MenuConfigOptions, MenuConfig> func)
+		{
+			var config = new MenuConfigOptions();
+			return func(config);
+
+			//return new MenuConfig
+			//        {
+			//            MenuItem = func(config).MenuItem,
+			//            DynamicMenu = func(config).DynamicMenu,
+			//            IsVisible = func(config).IsVisible
+			//        };
+		}
+
+	}
+
+
+	public interface IMenuConfigOptions
+	{
+		MenuItem AddStatic(string url, string label);
+		IEnumerable<MenuItem> AddDynamic(Func<MenuContext, IEnumerable<MenuItem>> func);
+		bool Visible(Func<MenuContext, MenuItem, bool> func);
+	}
+
+	public class MenuConfigOptions : IMenuConfigOptions
+	{
+		public MenuItem AddStatic(string url, string label)
+		{
+			return new MenuItem {Url = url, Label = label};
+		}
+
+		public IEnumerable<MenuItem> AddDynamic(Func<MenuContext, IEnumerable<MenuItem>> func)
+		{
+			return new List<MenuItem>();
+		}
+
+		public bool Visible(Func<MenuContext, MenuItem, bool> func)
+		{
+			return func(new MenuContext(), new MenuItem());
+		}
+	}
 }
--- TrainingTasks3/MenuConfig.cs
+++ TrainingTasks3/MenuConfig.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Collections.Generic;
+
 namespace TrainingTasks3
 {
-    public class MenuConfig
-    {
-    }
+	public class MenuConfig
+	{
+		public MenuItem MenuItem { get; set; }
+		public IEnumerable<MenuItem> DynamicMenu { get; set; }
+		public bool IsVisible { get; set; }
+	}
 }
