e677e4ac7d2f71044b55bae198da65b93b7dcb69
John Byrne <jbyrne@inlogik.com>
2012-08-08 10:13:50 +1000

    All Tests pass

--- TrainingTasks4/Html.cs
+++ TrainingTasks4/Html.cs
@@ -7,9 +7,9 @@ namespace TrainingTasks4
 {
 	public class Html
 	{
-		private string _tagName = "";
-		private IDictionary<string, string> _attributes = new Dictionary<string, string>();
-		private List<string> _classes = new List<string>();
+		private readonly string _tagName = "";
+		private readonly IDictionary<string, string> _attributes = new Dictionary<string, string>();
+		private readonly List<string> _classes = new List<string>();
 
 		public Html(string tag)
 		{
@@ -23,20 +23,11 @@ namespace TrainingTasks4
 		}
 		public Html Attr(string name, string value)
 		{
-			 //name + "=\"" + value + "\" ";
 			_attributes.Add(name, value);
 			return this;
 		}
 		public Html AddClass(string classname)
 		{
-			//if (classname.Contains("class="))
-			//{
-			//    return classname.Substring(0, classname.LastIndexOf("\"")) + classname +"\"";	
-			//}
-			//else
-			//{
-			//    return "class=\"" + classname + "\"";
-			//}
 			_classes.Add(classname);
 			return this;
 		}
@@ -47,26 +38,25 @@ namespace TrainingTasks4
 			return this;
 		}
 
-		public string ToString()
+		public new string ToString()
 		{
 			string tagClass = "";
 			string tagAttr = "";
 
 			if (_classes.Any())
 			{
-				tagClass = "class=\"" + String.Join(" ", _classes) + "\"";
+				tagClass = " class=\"" + String.Join(" ", _classes) + "\"";
 			}
 
 			if (_attributes.Any())
 			{
 				foreach (var item in _attributes)
 				{
-					tagAttr += item.Key + "=\"" + item.Value + "\"";
+					tagAttr += " "+item.Key + "=\"" + item.Value + "\"";
 				}
 			}
 
-			//return "<" + _tagName + " " + String.Join(" ", tagAttr) +" "+ tagClass + "></" + _tagName + ">";
-			return "<" + _tagName + " " + String.Join(" ", tagAttr) + tagClass + "></" + _tagName + ">";
+			return "<" + _tagName +  tagAttr + tagClass + "></" + _tagName + ">";
 		}
 
 		public static implicit operator string(Html html)
