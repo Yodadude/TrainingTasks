5da73c7c3fdec48f589040e420fdf0e966730ca3
John Byrne <jbyrne@inlogik.com>
2012-08-21 10:31:57 +1000

    Refactor of Html Helper InputFor

--- TrainingTasks4/Html.cs
+++ TrainingTasks4/Html.cs
@@ -8,9 +8,9 @@ namespace TrainingTasks4
 {
 	public class Html
 	{
-		private readonly string _tagName = "";
-		private readonly IDictionary<string, string> _attributes = new Dictionary<string, string>();
-		private readonly List<string> _classes = new List<string>();
+		protected string _tagName = "";
+		protected IDictionary<string, string> _attributes = new Dictionary<string, string>();
+		protected List<string> _classes = new List<string>();
 
 		public Html(string tag)
 		{
@@ -70,14 +70,11 @@ namespace TrainingTasks4
 	}
 
 
-	public class Html<T>
+	public class Html<T> : Html
 	{
-		private string _tagName = "";
-		private readonly IDictionary<string, string> _attributes = new Dictionary<string, string>();
-		private readonly List<string> _classes = new List<string>();
 		private readonly T _obj;
 
-		public Html(T obj)
+		public Html(T obj) : base("")
 		{
 			_obj = obj;
 		}
@@ -88,7 +85,7 @@ namespace TrainingTasks4
 			this._tagName = "input";
 			Attr("type", "text");
 
-			var body = ((LambdaExpression)expr).Body;
+			var body = expr.Body;
 			var memberExpression = body as MemberExpression;
 			if (memberExpression != null)
 			{
@@ -99,54 +96,11 @@ namespace TrainingTasks4
 			return this;
 		}
 
-		public Html<T> Attr(string name, string value)
-		{
-			_attributes.Add(name, value);
-			return this;
-		}
-
-		public Html<T> AddClass(string classname)
-		{
-			_classes.Add(classname);
-			return this;
-		}
-
-		public Html<T> Modify(Action<Html<T>> action)
-		{
-			action(this);
-			return this;
-		}
-
-		public new string ToString()
-		{
-			string tagClass = "";
-			string tagAttr = "";
-
-			if (_classes.Any())
-			{
-				tagClass = " class=\"" + String.Join(" ", _classes) + "\"";
-			}
-
-			if (_attributes.Any())
-			{
-				foreach (var item in _attributes)
-				{
-					tagAttr += " " + item.Key + "=\"" + item.Value + "\"";
-				}
-			}
-
-			return "<" + _tagName + tagAttr + tagClass + "></" + _tagName + ">";
-		}
-
-		public static implicit operator string(Html<T> html)
-		{
-			return html.ToString();
-		}
 	}
-
-
+	
 	public class UserDetails
 	{
 		public string Location	{ get; set; }
 	}
+
 }
